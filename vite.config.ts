92e6619b9cc5923f6723a86b24792f7a74426fec
Eivind <eivindh3@gmail.com>
2025-12-01 20:29:29 +0100

    build: update package name

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import path from 'node:path'
 import { crx } from '@crxjs/vite-plugin'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import zip from 'vite-plugin-zip-pack'
 import { createManifest } from './manifest.config'
@@ -42,7 +42,7 @@ export default defineConfig(({ mode }) => {
 
 function startCase(str: string) {
 	return str
-		.split(' ')
+		.split(/[-_\s]+/g)
 		.map(word => word.charAt(0).toUpperCase() + word.slice(1))
 		.join(' ')
 }
